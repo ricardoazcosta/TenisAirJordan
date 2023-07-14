c9e6538af2b3c235851440c681ae824bf98198b0
Ricardo Alexandre Costa <ricardozanete@gmail.com>
2023-07-14 17:24:30 -0300

    :tada: alteracao das cores do tenis+circulo

---
+++ script.js
@@ -0,0 +1,10 @@
+let imgem = document.querySelector(".nikeair");
+let circulo = document.querySelector(".circulo");
+
+function trocaImagem(endereco){
+    imgem.src = endereco
+}
+
+function trocaCor(cor){
+    circulo.style.background = cor
+}
