8d2f0e9293330d24832fa495d8fe9456d4a11d3c
Ricardo Alexandre Costa <ricardozanete@gmail.com>
2023-07-17 15:46:42 -0300

    :tada: add funcao troca imagem setInterval

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-let imgem = document.querySelector(".nikeair");
+let imagem = document.querySelector(".nikeair");
 let circulo = document.querySelector(".circulo");
 
+
 function trocaImagem(endereco){
-    imgem.src = endereco
+    imagem.src = endereco
 }
 
 function trocaCor(cor){
     circulo.style.background = cor
 }
+
+function trocaImagemPLayer(){
+    let imagemPlayer = document.getElementById("trocaPlayer");
+    if(imagemPlayer.style.opacity == 0) {
+        imagemPlayer.style.opacity = 1
+    }else{
+        imagemPlayer.style.opacity = 0
+    }
+}
+
+setInterval (function(){
+    trocaImagemPLayer()
+}, 4000);
